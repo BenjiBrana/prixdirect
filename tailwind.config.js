fa8784f91c4e60794cf564c621c357e8d1d2bf88
Benji Brana <benjidebran@gmail.com>
2023-09-22 16:42:55 +0200

    correction color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html"],
+  content: ['./public/index.html'],
   theme: {
     extend: {
       colors: {
-				primary: {
-					DEFAULT: '#f1f5f9',
-				},
-				secondary: {
-					DEFAULT: '#fcc300',
-				},
-				tertiary: {
-					DEFAULT: '#17c7be',
-				},
-			},
-			fontFamily: {
-				title: ['Open Sans'],
-				body: ['Roboto'],
-			},
-			borderRadius: {
-				'4xl': '3.5rem',
-			},
+        primary: {
+          DEFAULT: '#f1f5f9',
+        },
+        secondary: {
+          DEFAULT: '#fcc300',
+        },
+        tertiary: {
+          DEFAULT: '#17c7be',
+        },
+      },
+      fontFamily: {
+        title: ['Open Sans'],
+        body: ['Roboto'],
+      },
+      borderRadius: {
+        '4xl': '3.5rem',
+      },
     },
     container: {
-			center: true,
-		},
+      center: true,
+    },
   },
   variants: {
-		extend: {
-			scale: ['active'],
-			transform: ['active'],
-		},
-	},
+    extend: {
+      scale: ['active'],
+      transform: ['active'],
+    },
+  },
   plugins: [],
-}
-
+};
