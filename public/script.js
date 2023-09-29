762f55e463e75609280119f6da7d8b842f0d947f
Benji Brana <benjidebran@gmail.com>
2023-09-29 14:33:41 +0200

    modifs du script

--- public/script.js
+++ public/script.js
@@ -1,8 +1,13 @@
-function check() {
-let prix = document.getElementById('prix').value;
-let remise = document.getElementById('remise').value;
-let prixRemisee = (prix * remise)/100;
-let total = prix - prixRemisee;
 
-document.getElementById('resultat').innerHTML = total.toPrecision(4) + " €";
+function check() {
+    let prix = parseFloat(document.getElementById('prix').value);
+    let remise = parseFloat(document.getElementById('remise').value);
+
+    if (isNaN(prix) || isNaN(remise)) {
+        console.error("Les valeurs saisies ne sont pas valides.");
+        return;
+    }
+
+    let total = prix - (prix * remise / 100);
+    document.getElementById('resultat').innerHTML = total.toFixed(2) + " €";
 }
